0bfeed1b9959429ea4faba2770a35e4e7867ce70
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-08-05 09:56:35 +0530

    Initial Reposrting service and configurations

---
+++ reporting-service/src/test/java/com/vinod/b2b/ReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinod.b2b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
