8cf03b8bbe52844b96148f0c422bcdc946e1ffab
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-08-05 19:48:38 +0530

    feat(auth-service):add the AuthService and JWTUtill with methods

---
+++ auth-service/src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Users;
+import com.example.demo.repository.UserCredentialRepository;
+import com.example.demo.utility.Roles;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class AuthService {
+
+	private final UserCredentialRepository repository;
+	private final BCryptPasswordEncoder passwordEncoder;
+	private final JwtUtil jwtUtil;
+
+	public Users registerUser(Users users) {
+		Users user = new Users();
+		user.setEmail(users.getEmail());
+		user.setUsername(users.getUsername());
+		user.setRole(users.getRole());
+		user.setPassword(passwordEncoder.encode(users.getPassword()));
+		return repository.save(user);
+	}
+
+	public String generateToken(String username, Roles role) {
+		return jwtUtil.generateToken(username, role);
+	}
+
+	public void validateToken(String token) {
+		if (!jwtUtil.validateToken(token)) {
+			throw new RuntimeException("Token is invalid or expired");
+		}
+	}
+
+	public String login(String email, String password) {
+		Users user = repository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+		if (!passwordEncoder.matches(password, user.getPassword())) {
+			throw new RuntimeException("Invalid password");
+		}
+		String token = jwtUtil.generateToken(user.getUsername(), user.getRole());
+
+		return token;
+	}
+}
---
+++ auth-service/src/main/java/com/example/demo/service/JwtUtil.java
@@ -0,0 +1,62 @@
+package com.example.demo.service;
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.utility.Roles;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+	
+	public static String SECRET;
+	  @Value("${jwt.secret.key}")
+	    public void setSecret(String secret) {
+	        SECRET = secret;
+	    }
+	private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String username, Roles roles) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    public boolean validateToken(String token) {
+        final String extractedUsername = extractUsername(token);
+        return !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+}
