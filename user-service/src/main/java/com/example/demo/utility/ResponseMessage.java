54dc1add0ce4412526675e1b91d3969f015b2264
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-08-07 17:05:22 +0530

    feat(user-service): add the ResponseStatus and ResponseMEssage class for API response

---
+++ user-service/src/main/java/com/example/demo/utility/ResponseMessage.java
@@ -0,0 +1,26 @@
+package com.example.demo.utility;
+
+import lombok.Data;
+
+@Data
+public class ResponseMessage<T> {
+	private Integer statuscode;
+	private String status;
+	private String message;
+	private T data;
+
+	public ResponseMessage(Integer statuscode, String status, String message) {
+		super();
+		this.statuscode = statuscode;
+		this.status = status;
+		this.message = message;
+	}
+
+	public ResponseMessage(Integer statuscode, String status, String message, T data) {
+		super();
+		this.statuscode = statuscode;
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
+}
