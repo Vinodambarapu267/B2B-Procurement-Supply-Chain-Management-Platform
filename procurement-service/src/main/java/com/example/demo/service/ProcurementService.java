efd8cc7003368bcebe4d453d3edb20bd35430b51
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-08-10 18:44:57 +0530

    feat(procure-service): add the @service annotation on the top of interface

--- procurement-service/src/main/java/com/example/demo/service/ProcurementService.java
+++ procurement-service/src/main/java/com/example/demo/service/ProcurementService.java
@@ -3,8 +3,11 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.PurchaseRequest;
 
+@Service
 public interface ProcurementService {
 
 	PurchaseRequest createPurchaseRequest(PurchaseRequest purchaseRequest);
