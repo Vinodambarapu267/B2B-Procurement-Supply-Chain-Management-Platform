9a387b4974c1138c58178aff4d2691ab034e71bd
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-08-11 21:53:25 +0530

    feat(approve-repo): add the listAllPending from approver to repository

--- approval-service/src/main/java/com/vinod/b2b/repository/ApprovalRepository.java
+++ approval-service/src/main/java/com/vinod/b2b/repository/ApprovalRepository.java
@@ -1,14 +1,20 @@
 package com.vinod.b2b.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.vinod.b2b.entity.ApprovalDecisions;
-import java.util.List;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 public interface ApprovalRepository extends JpaRepository<ApprovalDecisions, UUID> {
 	ApprovalDecisions findByPrId(UUID id);
 
 	List<ApprovalDecisions> findAllByPrId(UUID prId);
+
+	@Query("SELECT a FROM ApprovalDecisions a WHERE a.approverId = :approverId")
+	List<ApprovalDecisions> findAllByApproverId(@Param("approverId") UUID approverId);
 }
