5301c073682c09016b1ce7502b16c36f09800b35
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-08-12 18:44:35 +0530

    feat(purchase-order-entity):add the constraints for each field

--- purchase-order-service/src/main/java/com/vinod/b2b/entity/PurchaseOrders.java
+++ purchase-order-service/src/main/java/com/vinod/b2b/entity/PurchaseOrders.java
@@ -2,8 +2,12 @@ package com.vinod.b2b.entity;
 
 import java.util.UUID;
 
+import org.aspectj.weaver.ast.Var;
+import org.springframework.web.jsf.FacesContextUtils;
+
 import com.vinod.b2b.utility.OrderStatus;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,11 +21,18 @@ import lombok.Data;
 public class PurchaseOrders {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(nullable = false)
 	private UUID purchaseOrderId;
+	@Column(nullable = false)
 	private UUID prId;
+	@Column(nullable = false)
 	private UUID supplierId;
-	private Integer poNumber;
+	@Column(nullable = false, unique = true)
+	private Var poNumber;
+	@Column(nullable = false)
 	private OrderStatus status;
+	@Column(nullable = false)
 	private Double totalAmount;
+	@Column(nullable = false)
 	private Integer version;
 }
