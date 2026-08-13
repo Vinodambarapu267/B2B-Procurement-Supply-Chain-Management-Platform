7b34ddaca72b6c5f51f0db537f6dbf1bd8d4efa7
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-08-13 19:04:23 +0530

    feat(supplier-controller): add ResponseEntity for API responses

--- supplier-service/src/main/java/com/vinod/b2b/controller/SupplierController.java
+++ supplier-service/src/main/java/com/vinod/b2b/controller/SupplierController.java
@@ -1,8 +1,11 @@
 package com.vinod.b2b.controller;
 
+import java.net.HttpURLConnection;
+import java.net.http.HttpClient;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vinod.b2b.entity.Supplier;
 import com.vinod.b2b.service.SupplierService;
+import com.vinod.b2b.utility.ResponseMessage;
+import com.vinod.b2b.utility.ResponseStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,13 +29,43 @@ public class SupplierController {
 	private final SupplierService supplierService;
 
 	@PostMapping
-	public Supplier createSupplier(@RequestBody Supplier supplier) {
-		return supplierService.createSupplier(supplier);
+	public ResponseEntity<?> createSupplier(@RequestBody Supplier supplier) {
+		 Supplier newSupplier = supplierService.createSupplier(supplier);
+		 if(newSupplier==null) {
+			 return ResponseEntity.ok(ResponseMessage.builder()
+					 .message("creating New Supplier failed...")
+					 .status(ResponseStatus.FAILED.name())
+					 .statusCode(HttpURLConnection.HTTP_NOT_ACCEPTABLE)
+					 .build()
+					 );
+		 }
+		 return ResponseEntity.ok(ResponseMessage.builder()
+				 .message("New Supplier Created Successfully ...")
+				 .status(ResponseStatus.SUCCESS.name())
+				 .statusCode(HttpURLConnection.HTTP_CREATED)
+				 .data(newSupplier)
+				 .build()
+				 );
 	}
 
 	@PatchMapping("/{id}/status")
-	public Supplier updateStatus(@PathVariable(name = "id") UUID supplierId) {
-		return supplierService.updateSupplierStatus(supplierId);
+	public ResponseEntity<?> updateStatus(@PathVariable(name = "id") UUID supplierId) {
+		 Supplier updateSupplierStatus = supplierService.updateSupplierStatus(supplierId);
+		 if(updateSupplierStatus==null) {
+			 return ResponseEntity.ok(ResponseMessage.builder()
+					 .message(" Supplier updatining failed...")
+					 .status(ResponseStatus.FAILED.name())
+					 .statusCode(HttpURLConnection.HTTP_NOT_ACCEPTABLE)
+					 .build()
+					 );
+		 }
+		 return ResponseEntity.ok(ResponseMessage.builder()
+				 .message("Supplier updated successfully ...")
+				 .status(ResponseStatus.SUCCESS.name())
+				 .statusCode(HttpURLConnection.HTTP_CREATED)
+				 .data(updateSupplierStatus)
+				 .build()
+				 );
 	}
 
 	@GetMapping
